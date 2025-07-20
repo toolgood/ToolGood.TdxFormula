8e7237317e6eb2759d79758d3338d2e9eedbb2e1
LinZhijun <toolgood@qq.com>
2025-07-20 11:52:30 +0800

    fix

--- ToolGood.TdxFormulaTest/Program.cs
+++ ToolGood.TdxFormulaTest/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using ToolGood.ReadyGo3;
+﻿using ToolGood.ReadyGo3;
 using ToolGood.TdxFormula;
 using static ToolGood.TdxFormula.TdxMath;
 
@@ -14,22 +12,22 @@ namespace ToolGood.TdxFormulaTest
             using (var helper = SqlHelperFactory.OpenDatabase(conn, SqlType.SQLite)) {
                 var kday = helper.Select<DbKDay>("order by date desc limit 420");
 
-                TdxNumber CLOSE = kday.Select(q => q.Close).ToArray();
-                TdxNumber HIGH = kday.Select(q => q.High).ToArray();
-                TdxNumber LOW = kday.Select(q => q.Low).ToArray();
-                TdxNumber OPEN = kday.Select(q => q.Open).ToArray();
-
-                int N = 9;
-                int M1 = 3;
-                int M2 = 3;
-
-                var RSV = (CLOSE - LLV(LOW, N)) / (HHV(HIGH, N) - LLV(LOW, N)) * 100;
-                var K = SMA(RSV, M1, 1);
-                var D = SMA(K, M2, 1);
-                var J = 3 * K - 2 * D;
-
+                KDJ(kday, 9, 3, 3);
 
             }
         }
+        private static void KDJ(List<DbKDay> kday, int N, int M1, int M2)
+        {
+            TdxNumber CLOSE = kday.Select(q => q.Close).ToArray();
+            TdxNumber HIGH = kday.Select(q => q.High).ToArray();
+            TdxNumber LOW = kday.Select(q => q.Low).ToArray();
+
+            var RSV = (CLOSE - LLV(LOW, N)) / (HHV(HIGH, N) - LLV(LOW, N)) * 100;
+            var K = SMA(RSV, M1, 1);
+            var D = SMA(K, M2, 1);
+            var J = 3 * K - 2 * D;
+        }
+
+
     }
 }
